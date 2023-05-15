269da7861aa071c240eb29724d5721ac5881a0af
Azizbek Isroilov <aziz16110904@gmail.com>
2023-05-15 14:45:46 +0500

    load more messages

--- app/Http/Livewire/Chat/Chatbox.php
+++ app/Http/Livewire/Chat/Chatbox.php
@@ -12,9 +12,11 @@ class Chatbox extends Component
     public $selectedConversation;
     public $receiverInstance;
     public $messages;
+    public $height;
     public $message_count;
+    public $messages_count;
     public $paginateVar = 10;
-    protected $listeners = ['loadConversation', 'pushMessage'];
+    protected $listeners = ['loadConversation', 'pushMessage','loadmore'];
 
     public function pushMessage($messageId)
     {
@@ -35,6 +37,32 @@ class Chatbox extends Component
 
        $this->dispatchBrowserEvent('chatSelected');
     }
+
+    function loadmore()
+    {
+
+        // dd('top reached ');
+        $this->paginateVar = $this->paginateVar + 10;
+        $this->messages_count = Message::where('conversation_id', $this->selectedConversation->id)->count();
+
+        $this->messages = Message::where('conversation_id',  $this->selectedConversation->id)
+            ->skip($this->messages_count -  $this->paginateVar)
+            ->take($this->paginateVar)->get();
+
+        $height = $this->height;
+        $this->dispatchBrowserEvent('updatedHeight', ($height));
+        # code...
+    }
+
+    function updateHeight($height)
+    {
+
+        // dd($height);
+        $this->height = $height;
+
+        # code...
+    }
+
 
     public function render()
     {
--- resources/views/livewire/chat/main.blade.php
+++ resources/views/livewire/chat/main.blade.php
@@ -1,62 +1,63 @@
 <div>
-    {{-- <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Chat') }}
-        </h2>
-    </x-slot>
-    <div class="test">Test</div> --}}
+
+
     <div class="chat_container">
+
         <div class="chat_list_container">
+
             @livewire('chat.chat-list')
+
         </div>
 
         <div class="chat_box_container">
+
             @livewire('chat.chatbox')
 
             @livewire('chat.send-message')
         </div>
     </div>
 
+
     <script>
         window.addEventListener('chatSelected', event => {
 
-if (window.innerWidth < 768) {
+            if (window.innerWidth < 768) {
 
-    $('.chat_list_container').hide();
-    $('.chat_box_container').show();
+                $('.chat_list_container').hide();
+                $('.chat_box_container').show();
 
-}
+            }
 
-$('.chatbox_body').scrollTop($('.chatbox_body')[0].scrollHeight);
-let height= $('.chatbox_body')[0].scrollHeight;
-//alert(height);
-window.livewire.emit('updateHeight',{
+            $('.chatbox_body').scrollTop($('.chatbox_body')[0].scrollHeight);
+        let height= $('.chatbox_body')[0].scrollHeight;
+    //alert(height);
+    window.livewire.emit('updateHeight',{
 
 height:height,
 
 
-});
-});
+    });
+        });
 
 
-$(window).resize(function() {
+        $(window).resize(function() {
 
-if (window.innerWidth > 768) {
-    $('.chat_list_container').show();
-    $('.chat_box_container').show();
+            if (window.innerWidth > 768) {
+                $('.chat_list_container').show();
+                $('.chat_box_container').show();
 
-}
+            }
 
-});
-
-
-$(document).on('click', '.return', function() {
-
-$('.chat_list_container').show();
-$('.chat_box_container').hide();
+        });
 
 
-});
+        $(document).on('click', '.return', function() {
+
+            $('.chat_list_container').show();
+            $('.chat_box_container').hide();
+
+
+        });
     </script>
 
 <script>
