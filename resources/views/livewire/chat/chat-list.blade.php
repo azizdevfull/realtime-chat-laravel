c3857e7fd2f6d403e7e0cbd59a46298e98114ea6
Azizbek Isroilov <aziz16110904@gmail.com>
2023-05-15 11:07:52 +0500

    ChatList added style

--- resources/views/livewire/chat/chat-list.blade.php
+++ resources/views/livewire/chat/chat-list.blade.php
@@ -10,6 +10,24 @@
    </div>
 
    <div class="chatlist_body">
-
+    <div class="chatlist_item">
+        <div class="chatlist_img_container">
+             <img src="https://picsum.photos/id/200/200/300" alt="">
+        </div>
+        <div class="chatlist_info">
+            <div class="top_row">
+                <div class="list_username">Azizdev</div>
+                <div class="date">2d</div>
+            </div>
+            <div class="bottom_row">
+                <div class="message_body text-truncate">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore maxime porro obcaecati tempore accusamus ea nihil, totam tempora est velit. Perspiciatis distinctio sequi quibusdam corrupti perferendis corporis voluptatibus quisquam laboriosam!
+                </div>
+                <div class="unread_count">
+                    56
+                </div>
+            </div>
+        </div>
+    </div>
    </div>
 </div>
