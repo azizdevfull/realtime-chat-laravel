593efeafde85d4f3925065d0427225d4e4774145
Azizbek Isroilov <aziz16110904@gmail.com>
2023-05-15 11:52:23 +0500

    create & show users completed

--- resources/views/livewire/chat/create-chat.blade.php
+++ resources/views/livewire/chat/create-chat.blade.php
@@ -1,7 +1,10 @@
 <div>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Users') }}
-        </h2>
-    </x-slot>
+    @foreach ($users as $user)
+
+
+
+    <ul class="list-group w-75 mx-auto mt-3 container-fluid">
+        <li class="list-group-item list-group-item-action">{{$user->name}}</li>
+    </ul>
+    @endforeach
 </div>
