6ec5339f4133b9f80258778fa0e8b0fcc044dfbd
Azizbek Isroilov <aziz16110904@gmail.com>
2023-05-15 10:55:27 +0500

    ChatList part 1 done

--- resources/views/livewire/chat/chat-list.blade.php
+++ resources/views/livewire/chat/chat-list.blade.php
@@ -1,6 +1,12 @@
 <div>
    <div class="chatlist_header">
+    <div class="title">
+        Chat
+    </div>
 
+    <div class="img_container">
+        <img src="https://picsum.photos/200/300" alt="">
+    </div>
    </div>
 
    <div class="chatlist_body">
