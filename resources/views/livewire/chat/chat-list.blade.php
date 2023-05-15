3750eef9ff3a138037a610a6b06e9baf30e40c0c
Azizbek Isroilov <aziz16110904@gmail.com>
2023-05-15 13:27:54 +0500

    updated users avatars

--- resources/views/livewire/chat/chat-list.blade.php
+++ resources/views/livewire/chat/chat-list.blade.php
@@ -5,7 +5,7 @@
     </div>
 
     <div class="img_container">
-        <img src="https://picsum.photos/200/300" alt="">
+        <img src="https://ui-avatars.com/api/?background=0D8ABC&color=fff&name={{auth()->user()->name}}" alt="">
     </div>
    </div>
 
@@ -15,7 +15,9 @@
 
        <div class="chatlist_item" wire:key='{{$conversation->id}}'  wire:click="$emit('chatUserSelected', {{$conversation}},{{$this->getChatUserInstance($conversation, $name = 'id') }})">
            <div class="chatlist_img_container">
-             <img src="https://picsum.photos/id/{{ $this->getChatUserInstance($conversation,$name='id') }}/200/300" alt="">
+
+            <img src="https://ui-avatars.com/api/?name={{$this->getChatUserInstance($conversation, $name = 'name')}}"
+            alt="">
             </div>
             <div class="chatlist_info">
                 <div class="top_row">
--- resources/views/livewire/chat/chatbox.blade.php
+++ resources/views/livewire/chat/chatbox.blade.php
@@ -8,8 +8,7 @@
        </div>
 
         <div class="img_container">
-            <img src="https://picsum.photos/id/{{ $receiverInstance->id }}/200/300" alt="">
-        </div>
+            <img src="https://ui-avatars.com/api/?name={{ $receiverInstance->name }}" alt="">        </div>
 
         <div class="name">
             {{ $receiverInstance->name }}
